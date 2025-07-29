b47dfd68de2a8b15f9d8e3127f4c213ce6276969
samworkzz <162050373+samworkzz@users.noreply.github.com>
2025-07-29 08:59:07 +0530

    Time: 235 ms (2.59%) | Memory: 30.5 MB (17.05%) - LeetSync

---
+++ 189-rotate-array/rotate-array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        
+        vector<int> rotated(n);
+        
+        for (int i = 0; i < n; ++i) {
+            rotated[(i + k) % n] = nums[i];
+        }
+    
+        nums = rotated;
+    
+        for (auto x : nums) {
+            cout << x << " ";
+        }
+    }
+};
